01a512cc47b73d6834ed6dac7d9fbb14c9c7b25d
Kimi <kimigao1986@gmail.com>
2013-12-26 02:20:00 +0800

    init mixture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mixture::Application.config.secret_key_base = 'c8b822f544d94de075cc612b60a5302441c9c5bcafb889317a9bf7326c52c79a637ab5ee0d5ac2740890aac3b73c1942b8ba2b75cc31df3190ef78db4a967ca8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mixture::Application.config.session_store :cookie_store, key: '_mixture_session'
